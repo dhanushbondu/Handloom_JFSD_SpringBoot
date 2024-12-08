ae0d47fd2e8b919446edcccad7428e58d53c6119
dhanush <bondudhanush@gmail.com>
2024-12-08 22:35:12 +0530

    added optnio for gender in user signup

--- clint/src/Components/Signup.jsx
+++ clint/src/Components/Signup.jsx
@@ -1,24 +1,26 @@
 import React, { useState } from "react";
-import { useNavigate ,Link} from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 import './Signup.css';
 import Navbar from "./Navbar1";
+
 function Signup() {
     const [uname, setUname] = useState("");
     const [email, setEmail] = useState("");
     const [pwd, setPwd] = useState(""); 
+    const [gender, setGender] = useState(""); // State to hold gender
     const navigate = useNavigate();
-
 
     const handleSubmit = (e) => {
         e.preventDefault();
-    
+
         const user = {
             uname: uname,
             email: email,
             pwd: pwd,
+            gender: gender, // Add gender to the user data
         };
-    
+
         axios.post('/users/insert-user', user)
             .then((response) => {
                 if (response.status === 201) {
@@ -26,6 +28,7 @@ function Signup() {
                     setUname("");
                     setEmail("");
                     setPwd("");
+                    setGender(""); // Clear gender selection
                     navigate("/");
                 }
             })
@@ -39,16 +42,14 @@ function Signup() {
                 }
             });
     };
-    
-    
 
     return (
         <>
-        <Navbar/>
+            <Navbar/>
             <div className="signup-container">
-
                 <form onSubmit={handleSubmit}>
-                <h1 style={{alignItems:"center"}}>Sign-up</h1>
+                    <h1 style={{alignItems: "center"}}>Sign-up</h1>
+
                     <label>
                         <b>Username:</b> 
                         <input 
@@ -58,8 +59,9 @@ function Signup() {
                             onChange={(e) => setUname(e.target.value)} 
                         />
                     </label>
+
                     <label>
-                       <b> Email:</b> 
+                        <b>Email:</b> 
                         <input 
                             name="email"
                             type="email" 
@@ -67,20 +69,47 @@ function Signup() {
                             onChange={(e) => setEmail(e.target.value)} 
                         />
                     </label>
+
                     <label>
-                      <b>Password: </b>
+                        <b>Password:</b>
                         <input 
                             type="password" 
                             value={pwd} 
                             onChange={(e) => setPwd(e.target.value)} 
                         />
                     </label>
+
+                    <div className="gender-container">
+                        <label className="gender-label">
+                            <input 
+                                className="gender-radio"
+                                type="radio" 
+                                name="gender" 
+                                value="m" 
+                                checked={gender === "m"}
+                                onChange={(e) => setGender(e.target.value)} 
+                            />
+                            Male
+                        </label>
+                        <label className="gender-label">
+                            <input 
+                                className="gender-radio"
+                                type="radio" 
+                                name="gender" 
+                                value="f" 
+                                checked={gender === "f"}
+                                onChange={(e) => setGender(e.target.value)} 
+                            />
+                            Female
+                        </label>
+                    </div>
+
                     <button type="submit">Signup</button>
                     <br />
-          <p>
-           Already having an account?{" "}
-            <Link to="/signin">Login</Link>
-          </p>
+                    <p>
+                        Already have an account?{" "}
+                        <Link to="/signin">Login</Link>
+                    </p>
                 </form>
             </div>
         </>
